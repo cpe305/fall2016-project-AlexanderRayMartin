bccec246f4dd8d396bf3823ee987bf1a9652e1da
Alex Martin <alexanderraymartin@gmail.com>
2016-10-06 03:20:18 -0700

    Maven fix

--- src/alexanderRayMartin/main/Main.java
+++ src/alexanderRayMartin/main/Main.java
@@ -3,7 +3,7 @@ package alexanderRayMartin.main;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello github! This is Alex!");
+		System.out.println("Hello github! This is Alex Martin!");
 	}
 
 }

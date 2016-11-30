44a62da00870c894c4dcdf1c106768dcf093fb92
Alex Martin <alexanderraymartin@gmail.com>
2016-11-29 22:32:56 -0800

    Bug fixes.

--- src/alexanderraymartin/main/MapEditor.java
+++ src/alexanderraymartin/main/MapEditor.java
@@ -5,12 +5,6 @@ import alexanderraymartin.searchalgorithm.Building;
 import alexanderraymartin.searchalgorithm.Graph;
 import alexanderraymartin.ui.Screen;
 
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.logging.Level;
-
 /**
  * @author Alex Martin.
  *
@@ -51,41 +45,6 @@ public class MapEditor {
    */
   public static void setEditMode(boolean value) {
     editMode = value;
-  }
-
-  /**
-   * Creates and saves a blank node map to a file.
-   * 
-   * @param rows The number of rows.
-   * @param cols The number of columns.
-   */
-  public static void createBlankNodeMap(int rows, int cols) {
-    File file = new File("src/mapNode_temp.txt");
-    FileWriter fw;
-    Main.getLogger().fine("Creating blank node map");
-    try {
-      fw = new FileWriter(file);
-      BufferedWriter bw = new BufferedWriter(fw);
-
-      bw.write(String.valueOf(rows));
-      bw.write("  ");
-      bw.write(String.valueOf(cols));
-      bw.newLine();
-
-      for (int y = 0; y < rows; y++) {
-        for (int x = 0; x < cols; x++) {
-          bw.write("-1 ");
-        }
-        if (y != rows - 1) {
-          bw.newLine();
-        }
-      }
-
-      bw.close();
-      fw.close();
-    } catch (IOException exception) {
-      Main.getLogger().log(Level.FINE, "Exception", exception);
-    }
   }
 
   /**
